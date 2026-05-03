396b03134195e778436b4a91602642e63e2fa742
Ahmed Abshir <ahmed.abshir91@gmail.com>
2026-05-03 20:26:12 +0100

    Adds styles for larger screens and fixes navigation issue

--- scripts/game.js
+++ scripts/game.js
@@ -82,7 +82,7 @@ gridItems.forEach(item => {
 
 //function to handle clicking home button
 export const handleHomeBtnClick = () => {
-    window.location.replace('/');
+    window.location.replace(new URL('../index.html', import.meta.url).href);
 }
 //Adds event listener to home button
 homeIcon.addEventListener('click', handleHomeBtnClick);
@@ -189,7 +189,7 @@ const reloadGame = () => {
 newGameBtn.addEventListener('click', reloadGame);
 
 homeBtn.addEventListener('click', () => {
-    window.location.replace('/')
+    window.location.replace(new URL('../index.html', import.meta.url).href);
 })
 
 //Event listener for next question button in questions section
